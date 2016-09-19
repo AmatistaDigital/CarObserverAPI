2bdff398d3661722f7db84d056d80c39b1c50ded
Alberto Cole <acpii2005@gmail.com>
2016-09-18 18:24:57 -0600

    tar process on deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 echo 'building application ...'
 cd ..
+ls
 tar -czvf CarObserverAPI.tar.gz CarObserverAPI
-scp CarObserverAPI.tar.gz root@104.131.150.56:~/CarObserverAPI
-ssh root@104.131.150.56 tar -xzvf ~/CarObserverAPI/CarObserverAPI.tar.gz
+scp CarObserverAPI.tar.gz root@104.131.150.56:~/
+ssh root@104.131.150.56 tar -xzvf ~/CarObserverAPI.tar.gz
 echo 'done building this'
